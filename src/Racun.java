3663eefcbcb770eefa985b510d654508594741dc
Elmin Hodzic <elmin.hodzic@hotmail.com>
2018-08-13 04:59:09 +0200

    drugi komit

--- src/Racun.java
+++ src/Racun.java
@@ -2,6 +2,46 @@
 public class Racun {
 	
 	
-	
+	private int brojRacuna;
+	private String ime;
+	private int brojKnjiga;
+
+	Racun() {
+
+	}
+
+	public Racun(int brojRacuna, String ime) {
+		this.brojRacuna = brojRacuna;
+		this.ime = ime;
+	}
+
+	public Racun(int brojRacuna, String ime, int brojKnjiga) {
+		this(brojKnjiga, ime);
+		this.brojKnjiga = brojKnjiga;
+	}
+
+	public int getBrojRacuna() {
+		return brojRacuna;
+	}
+
+	public void setBrojRacuna(int brojRacuna) {
+		this.brojRacuna = brojRacuna;
+	}
+
+	public String getIme() {
+		return ime;
+	}
+
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+
+	public int getBrojKnjiga() {
+		return brojKnjiga;
+	}
+
+	public void setBrojKnjiga(int brojKnjiga) {
+		this.brojKnjiga = brojKnjiga;
+	}
 
 }
